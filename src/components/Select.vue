c774020f466d6187516e5e46782b8ceadb1dcdc0
CCLo <mloucctw@gmail.com>
2023-01-16 18:34:25 +0800

    src/components/Select.vue

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -15,6 +15,7 @@ export default {
             b_hide_completed: false,
             todos: [],
             xurl: '',
+            todo_done: false,
         }
     },
     methods: {
@@ -75,11 +76,25 @@ export default {
             this.xurl = 'http://127.0.0.1:5000/todos/' + id;
             const res = await fetch(this.xurl);
             this.todos = await res.json();
-        }
+        },
+        async f_update_done(todo) {
+            todo.done = !(todo.done);
+            await axios.post('http://127.0.0.1:5000/update_todo',
+                {
+                    todo_done: todo.done,
+                    todo_id: todo.id
+                })
+                .catch((error) => console.log(error));
+            console.log("done:" + todo.done);
+            this.b_delcat = false;
+        },
     },
     created() {
         this.f_fetch_cat();
     },
+    computed: {
+
+    }
 }
 </script>
 
@@ -88,23 +103,26 @@ export default {
     <!-- <p> {{ cats }}</p> -->
     <!-- <p> {{ todos }}</p> -->
     <div class="mflex">
+        <!-- ----- ----- ----- ----- ----- ----- ----- ----- -----  left Category -->
         <div class="left">
             <h2> Todo Category </h2>
+            <!-- ----- ----- ----- ----- combobox  -->
             <select v-model="cat_selected" @change="f_reload_todo()" class="left">
                 <option v-for="cat in cats" :value="cat">
                     {{ cat.name }}
                 </option>
             </select>
-            <p>
-                <button @click="b_addcat = !b_addcat"> Add Cat </button>
-                <button @click="b_delcat = !b_delcat"> Del Cat </button>
-            </p>
+            <!-- ----- -----  ----- ----- add delete -->
+            <button @click="b_addcat = !b_addcat"> Add Cat </button>
+            <button @click="b_delcat = !b_delcat"> Del Cat </button>
+            <!-- ----- ----- ----- ----- add form -->
             <div v-if="b_addcat">
                 <form @submit.prevent="f_add_cat">
                     <input v-model="i_new_cat" />
                     <button>Add Cat</button>
                 </form>
             </div>
+            <!-- ----- ----- ----- ----- delete form -->
             <div v-if="b_delcat">
                 <form @submit.prevent="f_del_cat">
                     <input v-model="cat_selected.name" />
@@ -114,7 +132,7 @@ export default {
 
 
         </div>
-        <!-- ---------------------------   right size  ------------------ -->
+        <!--  ----- ----- ----- ----- ----- ----- ----- ----- ----- right todo list  -->
         <div class="right">
             <div>
                 <h2> Todo List </h2>
@@ -122,7 +140,8 @@ export default {
             <h3> {{ cat_selected.name }} </h3>
             <ul>
                 <li v-for="todo in todos" :value="todo">
-                    <input type="checkbox" v-model="todo.done" class="larger" />
+                    <input type="checkbox" v-if="!todo.done" @change="f_update_done(todo)" class="larger" />
+                    <input type="checkbox" v-if="todo.done" checked @change="f_update_done(todo)" class="larger" />
                     <button @click="f_del_todo(todo)" class="small">X</button>
                     <span :class="{ done: todo.done }"> &nbsp {{ todo.title }} </span>
                 </li>
